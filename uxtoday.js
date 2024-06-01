adbe71a8b980728faf2dca057f7369397dc31005
Abel Prayoga <145668303+Macetah@users.noreply.github.com>
2024-06-01 22:46:56 +0700

    Basically everything added

---
+++ uxtoday.js
@@ -0,0 +1,45 @@
+// Horizontal scroll
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollerContainer = document.querySelector('.scroller-container');
+    
+    scrollerContainer.addEventListener('wheel', function(e) {
+        if (e.deltaY !== 0) {
+            e.preventDefault();
+            scrollerContainer.scrollLeft += e.deltaY;
+        }
+    });
+
+    const items = document.querySelectorAll('.item');
+
+    function isInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function showItems() {
+        items.forEach(item => {
+            if (isInViewport(item)) {
+                item.style.opacity = '1';
+                item.style.transform = 'translateX(0)';
+            }
+        });
+    }
+
+    showItems();
+    window.addEventListener('scroll', showItems)
+
+    // Mobile warning
+    function isMobileDevice() {
+        return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    }
+
+    if (isMobileDevice()) {
+        alert("For the best experience, please switch to the desktop version of our website.");
+    }
+});
+
